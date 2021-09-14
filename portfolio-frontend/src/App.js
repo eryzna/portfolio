b9aee9e591e3bff30f463cb8fc7756b4b6110eda
Elana R <eryzna@gmail.com>
2021-09-13 22:32:22 -0400

    navbar

--- portfolio-frontend/src/App.js
+++ portfolio-frontend/src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import Nav from './components/Nav.js'
 import './App.css';
+import './bootstrap/bootstrap.min.css'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Nav />
   );
 }
 
