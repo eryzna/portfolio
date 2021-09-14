9267722d8825c2c6e902eb0f68449b106b36f7fc
Elana R <eryzna@gmail.com>
2021-09-14 16:07:57 -0400

    navbar and row one

--- portfolio-frontend/src/App.js
+++ portfolio-frontend/src/App.js
@@ -1,12 +1,18 @@
 //import logo from './logo.svg';
 import Nav from './components/Nav.js'
+import RowOne from './components/RowOne.js'
 import './App.css';
 import './bootstrap/bootstrap.min.css'
 
 
+
 function App() {
   return (
+    < >
     <Nav />
+    <RowOne />
+
+    </>
   );
 }
 
---
+++ portfolio-frontend/src/components/RowOne.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+export default class RowOne extends Component {
+    
+
+    render() {
+        
+      return (
+
+        <div class="container-fluid">
+
+            <div class="row mb-3 row-1">
+              <div class="col-md-3 themed-grid-col grid-border">.col-md-3</div>
+              <div class="col-md-6 themed-grid-col grid-border">.col-md-6</div>
+              <div class="col-md-3 themed-grid-col grid-border extend">.col-md-3</div>
+            </div>
+        </div>
+
+      )
+    }
+}
