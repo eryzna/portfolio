21791ac2bc3860575e660247296a00bcbde65f87
Elana R <eryzna@gmail.com>
2021-09-27 16:41:10 -0400

    label css

--- portfolio-frontend/src/components/RowOne.js
+++ portfolio-frontend/src/components/RowOne.js
@@ -16,6 +16,9 @@ export default class RowOne extends Component {
                 
               </div>
               <div class="col-md-6 themed-grid-col grid-border" id="r1-6">
+                <div class= "grid-border" id="grid-label">
+                  <h4>Development Projects</h4>
+                </div>
                 <CarouselOne />
               </div>
               <div class="col-md-3 themed-grid-col grid-border" id="col-3-2">.col-md-3</div>
