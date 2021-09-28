e17a04c2473675f21d2ba9c2fbca7a8e77d4ad20
Elana R <eryzna@gmail.com>
2021-09-28 13:36:09 -0400

    carousel one captions

--- portfolio-frontend/src/components/CarouselOne.js
+++ portfolio-frontend/src/components/CarouselOne.js
@@ -11,10 +11,10 @@ export default class CarouselOne extends Component {
         <div class="carousel-inner">
           <div class="carousel-item active">
 
-            <svg class="bd-placeholder-img carousel-one" width="100%" height="260px" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
+            <svg class="bd-placeholder-img carousel-one" width="100%" height="260px" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#e7e5df"/></svg>
     
-            <div class="container ">
-              <div class="carousel-caption text-start">
+            <div class="container carousel-hover">
+              <div class="carousel-caption text-start carousel-caption">
                 <h1>Philosophy Tree</h1>
                 <p>React/Ruby on Rails</p>
                 <p><a class="btn btn-lg btn-primary" href="#">Sign up today</a></p>
@@ -22,9 +22,8 @@ export default class CarouselOne extends Component {
             </div>
           </div>
           <div class="carousel-item">
-            <img class ="bd-placeholder-img" src="/IMG3-02.png" width="115%" height="260px"></img>
-            
-    
+          <svg class="bd-placeholder-img carousel-one" width="100%" height="260px" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#e7e5df"/></svg>
+
             <div class="container">
               <div class="carousel-caption">
                 <h1>Footprint</h1>
@@ -34,19 +33,19 @@ export default class CarouselOne extends Component {
             </div>
           </div>
           <div class="carousel-item">
-            <img class ="bd-placeholder-img" src="/CGradient-03.png" width="115%" height="260px"></img>
+          <svg class="bd-placeholder-img carousel-one" width="100%" height="260px" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#e7e5df"/></svg>
     
-            <div class="container">
-              <div class="carousel-caption text-end">
-                <h1>One more for good measure.</h1>
-                <p>Some representative placeholder content for the third slide of this carousel.</p>
-                <p><a class="btn btn-lg btn-primary" href="#">Browse gallery</a></p>
+            <div class="container carousel-hover">
+              <div class="carousel-caption text-start carousel-caption">
+                <h1>Monarch Monarchy</h1>
+                <p>JS/Ruby on Rails</p>
+                <p><a class="btn btn-lg btn-primary" href="#">Sign up today</a></p>
               </div>
             </div>
           </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="carousel-control-prev-icon" aria-hidden="true" id="c-1-next"></span>
           <span class="visually-hidden">Previous</span>
         </button>
         <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
